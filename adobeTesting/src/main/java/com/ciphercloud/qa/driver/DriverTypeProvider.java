0e29f06343ca9fe031699704cc978225c65d319b
bhupesh sharma <bboddupally@ciphercloud.com>
2016-03-22 16:55:09 +0530

    test

--- adobeTesting/src/main/java/com/ciphercloud/qa/driver/DriverTypeProvider.java
+++
@@ -1,9 +0,0 @@
-package com.ciphercloud.qa.driver;
-
-public abstract class DriverTypeProvider {
-	public static String browserName;
-	public abstract IDriverProvider getBrowserType();
-	public abstract  IBrowserProvider getDriverType();
-	
-
-}
